f2d0d5802f91b7aeed72401bf8f83058c9133ffb
NARARIA03 <hyeonseong0305@gmail.com>
2026-05-11 22:28:51 +0900

    chore(init): eslint 초기 설정 및 vscode 세팅 파일 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+const importSortConfig = {
+  files: ["**/*.{js,jsx,ts,tsx,mjs,mts}"],
+  plugins: {
+    "simple-import-sort": simpleImportSort,
+  },
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+};
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  importSortConfig,
+  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
