d4c2d859e90c1ac2a036be6bf1cd2aec983306ab
NARARIA03 <hyeonseong0305@gmail.com>
2026-05-13 00:45:11 +0900

    chore(ui): css 설정값 복사, 폰트 연결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import './globals.css';
 
+import { Montserrat } from 'next/font/google';
 import type { ReactNode } from 'react';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800'],
+  variable: '--font-montserrat',
+});
 
 type Props = {
   children: ReactNode;
@@ -9,7 +16,7 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className={`${montserrat.variable} antialiased`}>{children}</body>
     </html>
   );
 }
