f3f6ece042d4a009a9891486d00bc22824796965
NARARIA03 <hyeonseong0305@gmail.com>
2026-05-25 15:29:34 +0900

    fix(api): 무한 스크롤 시 isFetchingNextPage 조건 추가

--- src/views/post-list/ui/post-list-view.tsx
+++ src/views/post-list/ui/post-list-view.tsx
@@ -5,7 +5,7 @@ import { Observer } from '@/shared/ui/intersection-observer';
 import { PostCard } from '@/widgets/post-card';
 
 export function PostListView() {
-  const { data: postList, fetchNextPage } = useGetPostList({
+  const { isFetchingNextPage, data, fetchNextPage } = useGetPostList({
     params: {
       // TODO: useSearchParams를 통해 period, sort 등 주입
       size: DEFAULT_POST_LIST_SIZE,
@@ -13,10 +13,11 @@ export function PostListView() {
   });
 
   const handleObserverEnter = () => {
+    if (isFetchingNextPage) return;
     fetchNextPage();
   };
 
-  if (!postList || postList.length <= 0) {
+  if (!data || data.length <= 0) {
     return (
       <div className="flex flex-col items-center justify-center py-20">
         <p className="text-muted-foreground text-lg">조건에 맞는 게시물이 없습니다.</p>
@@ -27,7 +28,7 @@ export function PostListView() {
   // TODO: 다른 위젯 (정렬, 필터) 등 조립 필요
   return (
     <section className="mx-auto flex-1 px-4 py-6 md:max-w-182 md:px-6">
-      {postList.map((post) => (
+      {data.map((post) => (
         <PostCard key={post.id} post={post} />
       ))}
       <Observer onEnter={handleObserverEnter} />
