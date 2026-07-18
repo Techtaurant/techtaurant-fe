0ff21ad6077c87454e57379802c497efa8ae0537
NARARIA03 <hyeonseong0305@gmail.com>
2026-07-18 16:02:49 +0900

    remove(test): 환경 체크용 테스트 코드 삭제

--- src/shared/ui/__tests__/hello-world.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-describe('test environment', () => {
-  it('renders Hello, world!', () => {
-    render(<h1>Hello, world!</h1>);
-
-    expect(screen.getByRole('heading', { name: 'Hello, world!' })).toBeInTheDocument();
-  });
-});
